ce1cf4e201869aa12e406c2a0082a645f7ce5539
Yannick Croissant <k1der.country@free.fr>
2013-04-11 21:52:44 +0200

    add better logging examples

--- test/index-express.js
+++ test/index-express.js
@@ -6,15 +6,24 @@ var app = express();
 app.use(chromelogger.middleware);
 
 app.get('/', function(req, res) {
-  res.log('Log from ChromeLogger');
-  res.warn('Warning from ChromeLogger');
-  res.error('Error from ChromeLogger');
-  res.info('Info from ChromeLogger');
-  res.group('Group from ChromeLogger');
-  res.groupEnd('GroupEnd from ChromeLogger');
-  res.groupCollapsed('GroupCollapsed from ChromeLogger');
-  res.end('Hello World');
-  res.log('Fail from ChromeLogger');
+  res.log('Simple message');
+  res.log('Message', 'with', 4, 'parameters');
+  res.log('Message from Node.js %s', process.version);
+  res.log('Message with an Object', chromelogger);
+  res.warn('Warning message');
+  res.error('Error message');
+  res.info('Info message');
+  res.group('Grouped messages');
+  res.log('Message 1');
+  res.log('Message 2');
+  res.groupEnd();
+  res.groupCollapsed('Grouped messages (collapsed)');
+  res.log('Message 1');
+  res.log('Message 2');
+  res.groupEnd();
+  res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
+  res.end(JSON.stringify(res._ChromeLoggerData));
+  res.log('Attempt to log when the headers were already sent');
 });
 
 chromelogger.on('error', function(message) {
